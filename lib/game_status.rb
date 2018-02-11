d16382abfe04ce46f33b54df6c7e0f9de40b02a6
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-11 19:57:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won?(board)
   position_1 = board[win_index_1]
   position_2 = board[win_index_2]
   position_3 = board[win_index_3]
- 
+
   #combins both sections above
   #position_1 = board[win_combo[0]]
   #position_2 = board[win_combo[1]]
